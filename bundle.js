118d47f3d324f19237f29dd419f0a6fd4db5d696
ckroeger <ckroeger@users.noreply.github.com>
2024-08-01 18:04:20 +0000

    deploy: cb46cb43542ba4393ade939905913a26a691c1af

--- bundle.js
+++ bundle.js
@@ -1,2 +1,2 @@
-!function(){"use strict";console.log("Hello, World!")}();
+!function(e){"use strict";function o(){const e=document.querySelector(".burger-icon"),o=document.querySelector(".nav-menu");e.classList.toggle("open"),o.classList.toggle("open")}console.log("Hello, World!"),window.toggleMenu=o,e.toggleMenu=o,Object.defineProperty(e,"__esModule",{value:!0})}({});
 //# sourceMappingURL=bundle.js.map
