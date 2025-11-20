4dcfb0d5f8723c65f107da0faca51e0fe047e12f
ckroeger <ckroeger.hh@gmail.com>
2025-11-20 20:40:05 +0100

    add table data import page with overlay functionality and dynamic row generation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 
 console.log("Hello, World!");
-console.log(dayjs().format('{YYYY} MM-DD   HH:mm'))
+console.log(dayjs().format("{YYYY} MM-DD   HH:mm"));
 
 function toggleMenu() {
   const burgerIcon = document.querySelector(".burger-icon");
@@ -12,95 +12,85 @@ function toggleMenu() {
 
 window.toggleMenu = toggleMenu;
 
-const data = [
+sdata = [
   {
-    "Nr.": "01",
-    "HSN": "0080",
-    "TSN": "017",
+    HSN: "0080",
+    TSN: "017",
     "Leistung (kw)": "120",
-    "Kennzeichen": "S-WW 123",
-    "Saison": "12"
+    Kennzeichen: "S-WW 123",
+    Saison: "12",
   },
   {
-    "Nr.": "02",
-    "HSN": "0080",
-    "TSN": "017",
+    HSN: "0080",
+    TSN: "017",
     "Leistung (kw)": "120",
-    "Kennzeichen": "S-WW 234",
-    "Saison": "12"
+    Kennzeichen: "S-WW 234",
+    Saison: "12",
   },
   {
-    "Nr.": "03",
-    "HSN": "0203",
-    "TSN": "003",
+    HSN: "0203",
+    TSN: "003",
     "Leistung (kw)": "99",
-    "Kennzeichen": "S-WW 345",
-    "Saison": "8"
+    Kennzeichen: "S-WW 345",
+    Saison: "8",
   },
   {
-    "Nr.": "04",
-    "HSN": "0203",
-    "TSN": "003",
+    HSN: "0203",
+    TSN: "003",
     "Leistung (kw)": "99",
-    "Kennzeichen": "S-WW 456",
-    "Saison": "8"
+    Kennzeichen: "S-WW 456",
+    Saison: "8",
   },
   {
-    "Nr.": "05",
-    "HSN": "0203",
-    "TSN": "003",
+    HSN: "0203",
+    TSN: "003",
     "Leistung (kw)": "99",
-    "Kennzeichen": "S-WW 567",
-    "Saison": "8"
+    Kennzeichen: "S-WW 567",
+    Saison: "8",
   },
   {
-    "Nr.": "06",
-    "HSN": "0203",
-    "TSN": "003",
+    HSN: "0203",
+    TSN: "003",
     "Leistung (kw)": "99",
-    "Kennzeichen": "S-WW 678",
-    "Saison": "8"
+    Kennzeichen: "S-WW 678",
+    Saison: "8",
   },
   {
-    "Nr.": "07",
-    "HSN": "0203",
-    "TSN": "003",
+    HSN: "0203",
+    TSN: "003",
     "Leistung (kw)": "99",
-    "Kennzeichen": "S-WW 789",
-    "Saison": "8"
+    Kennzeichen: "S-WW 789",
+    Saison: "8",
   },
   {
-    "Nr.": "08",
-    "HSN": "0100",
-    "TSN": "001",
+    HSN: "0100",
+    TSN: "001",
     "Leistung (kw)": "175",
-    "Kennzeichen": "n/a",
-    "Saison": "6"
+    Kennzeichen: "n/a",
+    Saison: "6",
   },
   {
-    "Nr.": "09",
-    "HSN": "0100",
-    "TSN": "001",
+    HSN: "0100",
+    TSN: "001",
     "Leistung (kw)": "154",
-    "Kennzeichen": "n/a",
-    "Saison": "12"
+    Kennzeichen: "n/a",
+    Saison: "12",
   },
   {
-    "Nr.": "10",
-    "HSN": "0366",
-    "TSN": "007",
+    HSN: "0366",
+    TSN: "007",
     "Leistung (kw)": "75",
-    "Kennzeichen": "S-XY 1210",
-    "Saison": "12"
-  }
-]
+    Kennzeichen: "S-XY 1210",
+    Saison: "12",
+  },
+];
 
 // Für globalen Zugriff verfügbar machen
 window.vehicleData = data;
 
 // HSN-Ausgabe
 console.log("HSN-Liste:");
-data.forEach(item => {
+data.forEach((item) => {
   console.log(item.HSN);
 });
 
